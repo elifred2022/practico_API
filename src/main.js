ef25591092515b5c9929c80342d84cffb6714e2c
eliezer <elifred21@hotmail.com>
2022-07-21 20:13:29 -0300

    comit nates de migrar a axios para dejar el async de manera de ejemplo; segun el profesro axios es mejor y facil

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 
+// lista de trending: manipulacion del DOM con get y forEach
 async function getTrendingMoviesPreview() {
     const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
     const data = await res.json();
@@ -18,6 +19,32 @@ async function getTrendingMoviesPreview() {
         movieContainer.appendChild(movieImg);
         trendingPreviewMoviesContainer.appendChild(movieContainer);
 
+        
+    });
+ }
+
+ // lista de categoria: 
+async function getCategoriesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+  
+    const categories = data.genres;
+    categories.forEach(category => {
+        const PreviewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+        
+        const categoryContainer = document.createElement ('div');
+        categoryContainer.classList.add('category-container');
+
+        const categoryTitle = document.createElement('h3');
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id', 'id' + category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        PreviewCategoriesContainer.appendChild(categoryContainer);
+
+        
     });
  }
 
@@ -32,3 +59,4 @@ async function getTrendingMoviesPreview() {
 } */
 
 getTrendingMoviesPreview();
+getCategoriesPreview();
