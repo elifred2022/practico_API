141b39270b341655fb59467be0f15c71610a1b61
eliezer <elifred21@hotmail.com>
2022-07-21 20:59:01 -0300

    migrado el proyecto a Axios por recomendacion es mejor y mas ccomodo

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
+// Axios
+const api = axios.create({
+    baseURL: 'https://api.themoviedb.org/3/',
+    Headers: {
+        'content-Type': 'aplication/json;charset=utf-8',
+    },
+    params: {
+        'api_key': API_KEY,
+    },
+});
+
 
 // lista de trending: manipulacion del DOM con get y forEach
+
 async function getTrendingMoviesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY);
-    const data = await res.json();
-  
+    const { data } = await api('trending/movie/day');
     const movies = data.results;
+
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer = document.querySelector('#trendingPreview .trendingPreview-movieList')
                 
@@ -25,10 +36,9 @@ async function getTrendingMoviesPreview() {
 
  // lista de categoria: 
 async function getCategoriesPreview() {
-    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
-    const data = await res.json();
-  
+    const { data } = await api('genre/movie/list');
     const categories = data.genres;
+    
     categories.forEach(category => {
         const PreviewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
         
